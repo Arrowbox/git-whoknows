11304e38ca269abb1694f48677064ba07d4de5b0
Jayson Messenger <jayson.messenger@gmail.com>
2020-04-09 15:35:36 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# git-whoknows
-Find out who is familiar with a file
+# git-whoknows - Find "who knows" about a file
+
+# Synopsis:
+`git whoknows [<options>] <path>`
+
+# Description
+
+Describes who is likely familiar with a file
+
+# Options
+
+* `-L <lines>` - Specifically for a set of lines, can be specified multiple times
+* `--no-table/table` - Format output as an ascii table or comma-delimited
+* `--weight=<commits>,<lines>,<latest>,<earliest>` - Custom weightings for different metrics
+
+# Examples
+
+## Information about a file
+
+```
+> git whoknows src/main.rs
+name, email, score, commits, lines, latest, earliest
+Jayson Messenger, <jmessenger@gmail.com>, 12, 4, 10, 2020-04-10, 2019-02-01
+John Smith, <jsmith@gmail.com>, 10, 2, 12, 2019-01-01, 2019-01-01
+```
---
+++ src/main.rs
@@ -0,0 +1,137 @@
+
+use std::fmt;
+use git2::{BlameOptions, Repository, Signature, BlameHunk, Oid};
+use std::path::{PathBuf};
+use structopt::StructOpt;
+use std::collections::HashMap;
+
+#[derive(StructOpt)]
+#[allow(non_snake_case)]
+struct Args {
+    #[structopt(name = "path", parse(from_os_str))]
+    arg_path: PathBuf,
+    #[structopt(short = "M")]
+    /// find line moves within and across files
+    flag_M: bool,
+    #[structopt(short = "C")]
+    /// find line copies within and across files
+    flag_C: bool,
+    #[structopt(short = "F")]
+    /// follow only the first parent commits
+    flag_F: bool,
+}
+
+struct TrackedFile {
+    #[allow(dead_code)]
+    path: String,
+    owners: HashMap<String, Owner>
+}
+
+impl TrackedFile {
+    fn new (path: &String) -> TrackedFile
+    {
+        TrackedFile {
+            path: path.clone(),
+            owners: HashMap::new(),
+        }
+    }
+
+    fn add_hunk (
+        &mut self,
+        commit: &BlameHunk
+    )
+    {
+        let owner = Owner::new(&commit.final_signature());
+        self.owners.entry(owner.email.clone()).or_insert(owner).add_hunk(commit);
+    }
+}
+
+struct Owner {
+    #[allow(dead_code)]
+    name: String,
+    email: String,
+    commits: HashMap<Oid,usize>,
+}
+
+impl Owner {
+    fn new (sig: &Signature) -> Owner
+    {
+        let email = String::from_utf8_lossy(sig.email_bytes()).to_string();
+        let name = String::from_utf8_lossy(sig.name_bytes()).to_string();
+        Owner {
+            name,
+            email,
+            commits: HashMap::new(),
+        }
+    }
+
+    fn add_hunk (
+        &mut self,
+        commit: &BlameHunk
+    )
+    {
+        *self.commits.entry(commit.final_commit_id()).or_insert(0) += commit.lines_in_hunk();
+    }
+
+    fn lines (&self) -> usize
+    {
+       self.commits.values().sum::<usize>()
+    }
+
+}
+
+impl fmt::Display for Owner {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} <{}>: Lines: {} Count: {}",
+               self.name,
+               self.email,
+               self.lines(),
+               self.commits.len())
+    }
+
+}
+
+fn run(args: &Args) -> Result<(), git2::Error> {
+    //let path = Path::new(&args.arg_path[..]);
+    let repo = Repository::discover(&args.arg_path)?;
+
+    let path = args.arg_path.strip_prefix(
+        repo.path()
+            .parent()
+            .unwrap()
+        ).unwrap();
+
+    // Prepare our blame options
+    let mut opts = BlameOptions::new();
+    opts.track_copies_same_commit_moves(args.flag_M)
+        .track_copies_same_commit_copies(args.flag_C)
+        .first_parent(args.flag_F);
+
+    let mut tracker = TrackedFile::new(&path.display().to_string());
+
+    let blame = repo.blame_file(path, Some(&mut opts))?;
+
+    for hunk in blame.iter()
+    {
+        tracker.add_hunk(&hunk);
+    }
+
+    println!("File: {}", tracker.path);
+    let mut owners: Vec<&Owner> = tracker.owners.values().collect();
+    owners.sort_by(|a, b| b.lines().cmp(&a.lines()));
+
+    for owner in owners
+    {
+        println!("  {}", owner);
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let args = Args::from_args();
+    match run(&args) {
+        Ok(()) => {}
+        Err(e) => println!("error: {}", e)
+    }
+}
