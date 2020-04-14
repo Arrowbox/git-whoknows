4bd999d1b68a190e75ae232f43ca224442825947
Jayson Messenger <jayson.messenger@gmail.com>
2020-04-13 22:54:39 -0400

    Using a trait to get ready to allow swapping between using git2-rs and shelling out to git

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use git2::{BlameHunk, BlameOptions, Oid, Repository, Signature};
+use git2::{BlameHunk, BlameOptions, Repository};
 use std::collections::HashMap;
 use std::fmt;
 use std::path::PathBuf;
@@ -24,7 +24,6 @@ struct Args {
 }
 
 struct TrackedFile {
-    #[allow(dead_code)]
     path: String,
     owners: HashMap<String, Owner>,
 }
@@ -37,39 +36,58 @@ impl TrackedFile {
         }
     }
 
-    fn add_hunk(&mut self, commit: &BlameHunk) {
-        let owner = Owner::new(&commit.final_signature());
+    fn add_hunk(&mut self, hunk: &impl Hunk) {
+        let owner = Owner::new(hunk);
         self.owners
-            .entry(owner.email.clone())
+            .entry(hunk.email())
             .or_insert(owner)
-            .add_hunk(commit);
+            .add_hunk(hunk);
     }
 }
 
 struct Owner {
-    #[allow(dead_code)]
     name: String,
     email: String,
-    commits: HashMap<Oid, usize>,
+    commits: HashMap<String, usize>,
 }
 
 impl Owner {
-    fn new(sig: &Signature) -> Owner {
-        let email = String::from_utf8_lossy(sig.email_bytes()).to_string();
-        let name = String::from_utf8_lossy(sig.name_bytes()).to_string();
+    fn new(hunk: &impl Hunk) -> Owner {
         Owner {
-            name,
-            email,
+            name: hunk.author(),
+            email: hunk.email(),
             commits: HashMap::new(),
         }
     }
 
-    fn add_hunk(&mut self, commit: &BlameHunk) {
-        *self.commits.entry(commit.final_commit_id()).or_insert(0) += commit.lines_in_hunk();
+    fn add_hunk (&mut self, hunk: &impl Hunk) {
+        *self.commits.entry(hunk.sha1()).or_insert(0) += hunk.lines();
     }
 
     fn lines(&self) -> usize {
         self.commits.values().sum::<usize>()
+    }
+}
+
+trait Hunk {
+    fn sha1(&self) -> String;
+    fn author(&self) -> String;
+    fn email(&self) -> String;
+    fn lines(&self) -> usize;
+}
+
+impl Hunk for BlameHunk<'_> {
+    fn sha1(&self) -> String {
+        self.final_commit_id().to_string()
+    }
+    fn author(&self) -> String {
+        String::from_utf8_lossy(self.final_signature().name_bytes()).to_string()
+    }
+    fn email(&self) -> String {
+        String::from_utf8_lossy(self.final_signature().email_bytes()).to_string()
+    }
+    fn lines(&self) -> usize {
+        self.lines_in_hunk()
     }
 }
 
@@ -89,7 +107,6 @@ impl fmt::Display for Owner {
 fn main() -> Result<()> {
     let args = Args::from_args();
 
-    //let path = Path::new(&args.arg_path[..]);
     let repo = Repository::discover(&args.arg_path)?;
 
     // Construct the path relative to the Git repository.
